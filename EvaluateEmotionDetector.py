432d3fc78099774ffd91a2083c4dc44ee030f7bf
ShubJas <100420318+ShubJas@users.noreply.github.com>
2023-02-15 23:22:22 +0530

    Update EvaluateEmotionDetector.py

--- EvaluateEmotionDetector.py
+++ EvaluateEmotionDetector.py
@@ -29,34 +29,7 @@ test_generator = test_data_gen.flow_from_directory(
         color_mode="grayscale",
         class_mode='categorical')
 
-# # do prediction on test data
-# predictions = emotion_model.predict_generator(test_generator)
 
-# # see predictions
-# # for result in predictions:
-# #     max_index = int(np.argmax(result))
-# #     print(emotion_dict[max_index])
-
-# print("-----------------------------------------------------------------")
-# # confusion matrix
-# c_matrix = confusion_matrix(test_generator.classes, predictions.argmax(axis=1))
-# print(c_matrix)
-# cm_display = ConfusionMatrixDisplay(confusion_matrix=c_matrix, display_labels=emotion_dict)
-# cm_display.plot(cmap=plt.cm.Blues)
-# plt.show()
-
-# # Classification report
-# print("-----------------------------------------------------------------")
-# print(classification_report(test_generator.classes, predictions.argmax(axis=1)))
-
-
-
-
-# import numpy as np
-# from keras.models import model_from_json
-# import matplotlib.pyplot as plt
-# from keras.preprocessing.image import ImageDataGenerator
-# from sklearn.metrics import confusion_matrix, classification_report,ConfusionMatrixDisplay
 import tensorflow as tf
 
 
